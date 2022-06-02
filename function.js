23c5bf9c11e92d8d1e7f7d4b6a4fa65bd40e63fc
ZefanyaWijaya <64416388+ZefanyaWijaya@users.noreply.github.com>
2022-06-03 01:14:11 +0700

    tryAnotherDay

--- function.js
+++ function.js
@@ -392,7 +392,8 @@ function update_calculate_whey (score , id) {
 
         const query = util.promisify(connection.query).bind(connection);
 
-        const row = await query("UPDATE calculate_whey SET score_saw = ? WHERE id_whey_protein = ?" , [score,id]);
+        // const row = await query("UPDATE calculate_whey SET score_saw = ? WHERE id_whey_protein = ?" , [score,id]);
+        const row = await connection.execute("UPDATE calculate_whey SET score_saw = ? WHERE id_whey_protein = ?" , [score,id]);
 
         connection.release();
 
