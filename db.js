24c6fa180573f14b47707f3db01502ab04ece71e
ZefanyaWijaya <64416388+ZefanyaWijaya@users.noreply.github.com>
2022-06-08 18:35:54 +0700

    tryNewMethodInOtherDB

--- db.js
+++ db.js
@@ -1,18 +1,26 @@
 var mysql = require('mysql');
 const Connection = require('mysql/lib/Connection');
 
+// var con = mysql.createPool({
+//   host: "dbwisp.c5zujjjdmbwx.us-west-2.rds.amazonaws.com",
+//   user: "jleopold8",
+//   password: "m45t3r88",
+//   port : 3306, 
+//   database : "spk_saw_whey_protein" ,
+//   connectionLimit : 100,
+//   connectTimeout : 5000,
+//   timeout : 3000,
+//   multipleStatements : true,
+//   waitForConnections : true,
+//   acquireTimeout : 30000,
+// });
+
 var con = mysql.createPool({
-  host: "dbwisp.c5zujjjdmbwx.us-west-2.rds.amazonaws.com",
-  user: "jleopold8",
-  password: "m45t3r88",
+  host: "localhost",
+  user: "root",
+  password: "password",
   port : 3306, 
   database : "spk_saw_whey_protein" ,
-  connectionLimit : 100,
-  connectTimeout : 5000,
-  timeout : 3000,
-  multipleStatements : true,
-  waitForConnections : true,
-  acquireTimeout : 30000,
 });
 
 // con.connect(function(err) {
